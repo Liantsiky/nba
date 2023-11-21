fe8fbfe2b8dab15dd08a8981c05b034b357eedbb
Liantsiky <malalatsiky11@gmail.com>
2023-11-21 15:21:45 +0300

    Controllers

--- src/main/java/org/nba/controller/EquipeSaisonController.java
+++ src/main/java/org/nba/controller/EquipeSaisonController.java
@@ -1,10 +1,52 @@
 package org.nba.controller;
 
+import java.util.List;
+
+import org.nba.model.EquipeSaison;
+import org.nba.service.EquipeSaisonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping(path="equipesaison")
-public class EquipeSaisonController {
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+@RestController
+@RequestMapping(path="/api")
+public class EquipeSaisonController {
+	@Autowired
+	ObjectMapper objectMapper;
+	
+	@Autowired
+	EquipeSaisonService equipeSaisonService;
+	
+
+	@GetMapping("/equipe_saisons")
+	public ResponseEntity<String> getAllequipeSaisons() {
+		try {
+			List<EquipeSaison> liste = equipeSaisonService.getAll();
+			
+			String json = objectMapper.writeValueAsString(liste);
+			return new ResponseEntity<>(json,HttpStatus.OK);
+		} catch (Exception e) {
+			// TODO: handle exception
+			return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+	
+
+	@PostMapping("/equipe_saisons")
+	public ResponseEntity<String> createEquipeSaison(@RequestBody EquipeSaison equipeSaison){
+		try {
+			EquipeSaison _equipeSaison = equipeSaisonService.insert(equipeSaison);
+			String json = objectMapper.writeValueAsString(_equipeSaison);
+			return new ResponseEntity<>(json,HttpStatus.CREATED);
+		} catch(Exception e) {
+			return new ResponseEntity<> (e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
 }
--- src/main/java/org/nba/controller/StatistiqueController.java
+++ src/main/java/org/nba/controller/StatistiqueController.java
@@ -1,10 +1,54 @@
 package org.nba.controller;
 
+import java.util.List;
+
+import org.nba.model.Saison;
+import org.nba.model.Statistique;
+import org.nba.service.SaisonService;
+import org.nba.service.StatistiqueService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping(path="statistique")
-public class StatistiqueController {
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+@RestController
+@RequestMapping(path="/api")
+public class StatistiqueController {
+	@Autowired
+	ObjectMapper objectMapper;
+	
+	@Autowired
+	StatistiqueService statistiqueService;
+	
+
+	@GetMapping("/statistiques")
+	public ResponseEntity<String> getAllStatistiques() {
+		try {
+			List<Statistique> liste = statistiqueService.getAll();
+			
+			String json = objectMapper.writeValueAsString(liste);
+			return new ResponseEntity<>(json,HttpStatus.OK);
+		} catch (Exception e) {
+			// TODO: handle exception
+			return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+	
+
+	@PostMapping("/statistiques")
+	public ResponseEntity<String> createStatistique(@RequestBody Statistique statistique){
+		try {
+			Statistique _statistique = statistiqueService.insert(statistique);
+			String json = objectMapper.writeValueAsString(_statistique);
+			return new ResponseEntity<>(json,HttpStatus.CREATED);
+		} catch(Exception e) {
+			return new ResponseEntity<> (e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
 }
